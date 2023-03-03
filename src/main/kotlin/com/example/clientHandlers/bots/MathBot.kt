c0866a98b71f8fe829a12e4a2a535aba4afbecf5
Yoav Marco <yoavm448@gmail.com>
2023-03-03 22:48:46 +0200

    Move MathBot to new API

--- src/main/kotlin/com/example/clientHandlers/bots/MathBot.kt
+++ src/main/kotlin/com/example/clientHandlers/bots/MathBot.kt
@@ -1,35 +1,28 @@
 package com.example.clientHandlers.bots
 
-import com.example.server.ChatServer
+val MathBot = newBot("MathBot") {
+    addMethod(Bot.Method("list", listOf("n"), "Print n, n-1, ..., 1") { _, args ->
+        val n = args[0].toInt()
+        if (n != 0) {
+            broadcastMsg("$botPrefix$name list ${n - 1}")
+        }
+    })
 
-class MathBot(server: ChatServer) : Bot("MathBot", server) {
-    init {
-        addMethod(Method("list", listOf("n"), "Print n, n-1, ..., 1") { _, args ->
-            val n = args[0].toInt()
-            if (n != 0) {
-                broadcastMsg("$botPrefix$name list ${n - 1}")
+    addMethod(Bot.Method("factorial", listOf("n"), "Calculate n!") { _, args ->
+        val n = args[0].toInt()
+        broadcastMsg("$botPrefix$name _factorial $n 1")
+    })
+    addMethod(
+        Bot.Method(
+            "_factorial",
+            listOf("n", "acc"),
+            "Internal function of factorial"
+        ) { _, args ->
+            val (n, acc) = args.map(String::toInt)
+            if (n == 0) {
+                broadcastMsg("Result is $acc")
+            } else {
+                broadcastMsg("$botPrefix$name _factorial ${n - 1} ${acc * n}")
             }
         })
-
-        addMethod(Method("factorial", listOf("n"), "Calculate n!") { _, args ->
-            val n = args[0].toInt()
-            broadcastMsg("$botPrefix$name _factorial $n 1")
-        })
-        addMethod(
-            Method(
-                "_factorial",
-                listOf("n", "acc"),
-                "Internal function of factorial"
-            ) { _, args ->
-                val (n, acc) = args.map(String::toInt)
-                if (n == 0) {
-                    broadcastMsg("Result is $acc")
-                } else {
-                    broadcastMsg("$botPrefix$name _factorial ${n - 1} ${acc * n}")
-                }
-            })
-    }
-    companion object: BotConstructor {
-        override fun new(server: ChatServer) = MathBot(server)
-    }
 }
