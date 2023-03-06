224afe79e2155d2e52e1121c6d80398606cb4f50
Yoav Marco <yoavm448@gmail.com>
2023-03-06 10:40:37 +0200

    Move Command to UserInput

--- src/main/kotlin/com/example/hubClients/websocketClients/UserInput.kt
+++ src/main/kotlin/com/example/hubClients/websocketClients/UserInput.kt
@@ -1,31 +1,51 @@
 package com.example.hubClients.websocketClients
 
 import com.example.sendln
+import com.example.server.ChatMessage
 import com.example.splitSpaces
 
-sealed class UserInput(open val owner: AuthenticatedClient) {
+sealed class UserInput(val owner: AuthenticatedClient) {
     abstract suspend fun execute()
-    class Msg(private val str: String, override val owner: AuthenticatedClient) :
-        UserInput(owner) {
+    class Msg(private val str: String, owner: AuthenticatedClient) : UserInput(owner) {
         override suspend fun execute() {
             owner.broadcastMsg(str)
         }
     }
 
-    class Cmd(
-        private val cmd: Command,
-        private val args: List<String>,
-        override val owner: AuthenticatedClient
+    sealed class Cmd(
+        val args: List<String>,
+        owner: AuthenticatedClient
     ) : UserInput(owner) {
-        override suspend fun execute() {
-            cmd.execute(owner, args).onFailure { owner.sendln("$it") }
+
+        class Whisper(args: List<String>, owner: AuthenticatedClient) :
+            Cmd(args, owner) {
+            companion object {
+                const val name = "whisper"
+            }
+
+            override suspend fun execute() {
+                if (args.count() < 2) {
+                    return owner.sendln("Usage: whisper USER MSG...\n")
+                }
+                val recipient = args.first()
+                val msgContent = args.drop(1).joinToString(separator = " ")
+                val recipientClient = owner.server.hub.activeConnections[recipient]
+                val msg = ChatMessage(sender = owner.username, content = msgContent)
+                if (recipientClient != null) {
+                    recipientClient.incomingMsgs.emit(msg)
+                } else {
+                    owner.sendln("User isn't online")
+                }
+            }
         }
+
     }
 
-    class UnknownCmd(private val name: String, override val owner: AuthenticatedClient) :
+    class UnknownCmd(private val name: String, owner: AuthenticatedClient) :
         UserInput(owner) {
         override suspend fun execute() {
             owner.sendln("Invalid command: $name")
+
         }
     }
 }
@@ -45,16 +65,17 @@ class InputFactory(
 
     fun new(str: String): UserInput {
         return if (isCommand(str)) {
-            newCmd(str)
+            newCmd(str, owner)
         } else {
             UserInput.Msg(str, owner)
         }
     }
 
-    private fun newCmd(str: String): UserInput {
+    private fun newCmd(str: String, owner: AuthenticatedClient): UserInput {
         val (cmdStr, args) = splitToCmdAndArgs(str)
-        val cmd = Command.fromStringWithoutCmdPrefix(cmdStr)
-            ?: return UserInput.UnknownCmd(str, owner)
-        return UserInput.Cmd(cmd, args, owner)
+        return when (cmdStr) {
+            UserInput.Cmd.Whisper.name -> UserInput.Cmd.Whisper(args, owner)
+            else -> UserInput.UnknownCmd(cmdStr, owner)
+        }
     }
 }
